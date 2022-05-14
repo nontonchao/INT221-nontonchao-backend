30376621ee15160a99dbe65b5264bc18478782aa
itsmeny <68806699+itsmeny@users.noreply.github.com>
2022-05-14 23:56:50 +0700

    future time validation

--- src/main/java/com/example/oasip_back_nontonchao/controllers/EventController.java
+++ src/main/java/com/example/oasip_back_nontonchao/controllers/EventController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.List;
 
 @RestController
@@ -22,6 +24,7 @@ public class EventController {
     public EventPage getAllEvent(@RequestParam(defaultValue = "") int page) {
         return service.getEvents(page);
     }
+
 
     @PutMapping("/edit")
     public ResponseEntity editEvent(@RequestBody Event update) {
@@ -57,9 +60,12 @@ public class EventController {
 
     @PostMapping("")
     public ResponseEntity createEvent(@RequestBody Event req) {
+        Pattern p = Pattern.compile("^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,24}))$", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(req.getBookingEmail());
+        boolean isValidEmail = m.find();
         if (req.getBookingName().length() <= 0 || req.getBookingEmail().length() <= 0 || req.getEventStartTime().toString().length() <= 0) {
             return new ResponseEntity("Missing some field!", HttpStatus.BAD_REQUEST);
-        } else if (!req.getBookingEmail().contains("@")) {
+        } else if (!isValidEmail) {
             return new ResponseEntity("Invalid email!", HttpStatus.BAD_REQUEST);
         } else if (Instant.now().minusSeconds(60).isAfter(req.getEventStartTime())) {
             return new ResponseEntity("Please future time!", HttpStatus.BAD_REQUEST);
