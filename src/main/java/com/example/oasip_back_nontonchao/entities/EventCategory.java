35d8afcf906bd77987bd8be1f1c62147f5545cd7
itsmeny <chokjaroen.estk@mail.kmutt.ac.th>
2022-05-19 21:02:08 +0700

    eventDuration validation , eventCategoryName length 45 -> 100

--- src/main/java/com/example/oasip_back_nontonchao/entities/EventCategory.java
+++ src/main/java/com/example/oasip_back_nontonchao/entities/EventCategory.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Table(name = "event_category", indexes = {
         @Index(name = "eventCategoryName", columnList = "eventCategoryName", unique = true)
@@ -19,14 +19,16 @@ public class EventCategory {
     private Integer id;
 
     @NotBlank(message = "eventCategoryName shouldn't be blank or null")
-    @Length(min = 1, max = 100, message = "size must be between 1 and 45")
-    @Column(name = "eventCategoryName", nullable = false, length = 45)
+    @Length(min = 1, max = 100, message = "size must be between 1 and 100")
+    @Column(name = "eventCategoryName", nullable = false, length = 100)
     private String eventCategoryName;
 
     @Length(min = 0, max = 500, message = "size must be between 0 and 500")
     @Column(name = "eventCategoryDescription", length = 500)
     private String eventCategoryDescription;
 
+    @Min(1)
+    @Max(480)
     @Column(name = "eventDuration", nullable = false)
     private Integer eventDuration;
 
