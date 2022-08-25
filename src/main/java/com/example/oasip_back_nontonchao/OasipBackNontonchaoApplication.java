5a7e89bf0eda4d289c1f1d75741cac9ceec8310c
itsmeny <chokjaroenp501@gmail.com>
2022-08-25 15:50:25 +0700

    Change argon2 (spring security) -> argon2-jvm , remove unused import , code

--- src/main/java/com/example/oasip_back_nontonchao/OasipBackNontonchaoApplication.java
+++ src/main/java/com/example/oasip_back_nontonchao/OasipBackNontonchaoApplication.java
@@ -2,7 +2,6 @@ package com.example.oasip_back_nontonchao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @SpringBootApplication
 public class OasipBackNontonchaoApplication {
--- src/main/java/com/example/oasip_back_nontonchao/services/UserService.java
+++ src/main/java/com/example/oasip_back_nontonchao/services/UserService.java
@@ -1,19 +1,19 @@
 package com.example.oasip_back_nontonchao.services;
 
-import com.example.oasip_back_nontonchao.dtos.EventGet;
 import com.example.oasip_back_nontonchao.dtos.UserGet;
 import com.example.oasip_back_nontonchao.entities.User;
 import com.example.oasip_back_nontonchao.repositories.UserRepository;
 import com.example.oasip_back_nontonchao.utils.ListMapper;
-import org.hibernate.boot.model.source.internal.hbm.CompositeIdentifierSingularAttributeSourceManyToOneImpl;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 
 @Service
@@ -28,17 +28,17 @@ public class UserService {
     private ListMapper listMapper;
 
     public List<UserGet> getAllUsers() {
-        return listMapper.mapList(userRepository.findAll(Sort.by(Sort.Direction.ASC, "name")),UserGet.class,modelMapper);
+        return listMapper.mapList(userRepository.findAll(Sort.by(Sort.Direction.ASC, "name")), UserGet.class, modelMapper);
     }
 
     public ResponseEntity createUser(User user) {
         if (!isUniqueCreate(user.getName().trim(), user.getEmail().trim())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("this name or this email already taken!");
         } else {
-           Argon2PasswordEncoder s = new Argon2PasswordEncoder();
-           String encryptedPassword = s.encode(user.getPassword());
-           userRepository.createUser(user.getName(), user.getEmail(), user.getRole() ,encryptedPassword);
-           return ResponseEntity.status(HttpStatus.CREATED).body("user " + user.getName() + " created!");
+            Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id, 8, 32);
+            String passwordHash = argon2.hash(22, 65536, 1, user.getPassword());
+            userRepository.createUser(user.getName(), user.getEmail(), user.getRole(), passwordHash);
+            return ResponseEntity.status(HttpStatus.CREATED).body("user " + user.getName() + " created!");
         }
     }
 
