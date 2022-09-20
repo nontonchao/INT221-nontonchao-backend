0fd1f8f424b20db69e85a16b8268cb1e0bb40c67
itsmeny <chokjaroenp501@gmail.com>
2022-09-20 16:40:13 +0700

    setAllowedMethods

--- src/main/java/com/example/oasip_back_nontonchao/config/WebSecurityConfig.java
+++ src/main/java/com/example/oasip_back_nontonchao/config/WebSecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
@@ -50,9 +52,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and()
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        httpSecurity.csrf().disable().cors().configurationSource(request -> corsConfiguration
+                        .applyPermitDefaultValues()).and()
                 //.authorizeRequests().antMatchers("/api").authenticated().antMatchers("/**").permitAll().
-                .authorizeRequests().antMatchers("/api/users/create","/api/login","/api/events","/api/events-category","/api/events/date/***/***","/api/events/***","/api/users/check").permitAll()
+                .authorizeRequests().antMatchers("/api/users/create","/api/login","/api/events","/api/events-category","/api/events/date/***/***","/api/events/***","/api/users/check","/api/events/delete/***").permitAll()
                     .antMatchers("/adminonly","/api/users","/api/users/***").hasRole("ADMIN").anyRequest().authenticated().and().
                 //anyRequest().authenticated().and().
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
